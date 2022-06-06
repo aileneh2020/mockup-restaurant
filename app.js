9d8b2cb6ad9ab068e75ea373b816cc828a07f29a
Ailene-Hoang <aileneh2020@gmail.com>
2022-06-05 22:45:21 -0700

    add scroll to top button

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav");
 const navLinks = document.querySelectorAll(".nav-link");
+const scrollBtn = document.getElementById("scroll-top");
 
 // open and close mobile menu
 hamburger.addEventListener("click", () => {
@@ -8,7 +9,7 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 })
 
-// closes mobile menu when menu item clicked
+// close mobile menu when menu item clicked
 navLinks.forEach(navLink => {
   navLink.addEventListener("click", () => {
     if (hamburger.classList.contains("active")) {
@@ -16,4 +17,19 @@ navLinks.forEach(navLink => {
       navMenu.classList.toggle("active")
     }
   })
+})
+
+// show scroll button when user scrolls 200px from top
+document.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+})
+
+// scroll to top of page when scroll button clicked
+scrollBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 })
